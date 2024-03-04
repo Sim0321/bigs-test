78a695a7701d7fac92373ac02e3a8c0f83618561
Sim <0doogadooga@gmail.com>
2024-03-04 18:25:24 +0900

    css 초기화 및 globalStyle

---
+++ src/Global/theme.tsx
@@ -0,0 +1,7 @@
+export const theme = {
+  color: {},
+  backGround: {},
+  transition: {
+    default: "all 150ms cubic-bezier(0.4, 0, 0.2, 1)",
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@/App";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyle } from "@/Global/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
     </Router>
   </React.StrictMode>
 );
